9c5225999258e5e39648acbec6527e724637ecc5
Hemavathy S <158808035+Hema-vathy@users.noreply.github.com>
2025-11-01 11:26:48 +0530

    Create Package file

---
+++ TestBench/Pkg.sv
@@ -0,0 +1,10 @@
+/////////////////////***********************PACKAGE FOR PARAMETERS***********************/////////////////////
+package mux_pkg;
+
+  typedef struct {
+    int WIDTH;
+    int DEPTH;
+    int SELECT_LINE_DEPTH;
+  } mux_config_t;
+
+endpackage
